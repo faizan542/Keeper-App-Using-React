fe04e9c79dc7ecbc80311874cfd9aba080f1f815
faizan542 <faizansilat100@gmail.com>
2022-09-17 16:56:01 +0500

    Notes

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Notes() {
+  return (
+    <div>
+      <p>Notes</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus vel dolorum nobis explicabo sed dolore blanditiis reiciendis dolores nostrum, veritatis quod optio maiores ad natus incidunt. Fugiat commodi nobis sed.</p>
+    </div>
+  )
+}
+
+export default Notes
+
+// font-family: 'Righteous', cursive;   note title font family
+// font-family: 'Parisienne', cursive;  note para font family
