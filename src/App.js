fe1cfe317debc6424c445db8e0bc9165d4490d0f
faizan542 <faizansilat100@gmail.com>
2022-10-03 17:49:14 +0500

    App File

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import Header from './components/Header';
 import Notes from './components/Notes';
 import Footer from './components/Footer';
+import notes from './JS_file/notes';
+
+
+
 
 function App() {
   return (
     <div className="page-container">
-        <Header />
-        <Notes />
-
-      <Footer />
-
+      <Header />
+      <div className='row'>
+        {notes.map(noteItem => {
+          return (
+            <Notes key={noteItem.key} title={noteItem.title} content={noteItem.content} />
+          )
+        })}
     </div>
+        <Footer />
+      </div>
   );
 }
 
