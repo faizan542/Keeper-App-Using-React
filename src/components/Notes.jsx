95ddf9ab4d24bc82256d6c4aec586691021cdfaf
faizan542 <faizansilat100@gmail.com>
2022-10-03 17:48:17 +0500

    Notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
-function Notes() {
+function Notes(props) {
   return (
     <div className='note'>
-      <h1>Note Title</h1>
-      <p>This is a paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quia explicabo, voluptates reiciendis nesciunt cumque quam placeat laboriosam assumenda debitis, recusandae, repellat id enim nisi veritatis minus provident. Error, qui.</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
+
   )
 }
 
 export default Notes
-
-// font-family: 'Righteous', cursive;   note title font family
-// font-family: 'Parisienne', cursive;  note para font family
